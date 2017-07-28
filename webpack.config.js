e839dce600a059dd66f7f8ff50a1fd11a1a3da48
anirvan90 <anirvan.awatramani@gmail.com>
2017-07-28 15:56:37 +0530

    react renders

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './src/index.jsx'),
+  output: {
+    path: path.join(`${__dirname}/dist`),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: '/node_modules/',
+        test: /\.jsx?$/,
+        use: [{ loader: 'babel-loader' }]
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
